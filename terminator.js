d5c71e1046b0fa334ed8f0a087169ea10fa8969a
JC/BC <analytalica@gmail.com>
2016-05-23 18:25:58 -0500

    First pass at auto-type feature

--- terminator.js
+++ terminator.js
@@ -7,17 +7,19 @@ var Terminator = function(element, config) {
     this.element = element;
     this.hiddenField = hiddenField;
     this.config = config || {};
+    
     this.displayField = null;
+    this.locked = false;
     
     hiddenField.addEventListener('input', (function() {
         console.log('Typing!');
-        if (this.displayField) {
+        if (this.displayField && !this.locked) {
             this.displayField.textContent = this.hiddenField.value;
         }
     }).bind(this));
     
     hiddenField.addEventListener('keydown', (function(e) {
-        if (e.key == 13 || e.keyCode == 13) {
+        if ((e.key == 13 || e.keyCode == 13) && !this.locked) {
             console.log("Enter key pressed!");
             this.lineBreak();
             this.hiddenField.value = '';
@@ -35,6 +37,28 @@ var Terminator = function(element, config) {
 Terminator.prototype.lineBreak = function() {
     var br = document.createElement('br');
     this.element.appendChild(br);
+}
+
+Terminator.prototype.autoType = function(command) {
+    this.locked = true;
+    this.displayField.textContent = '';
+    
+    var funcs = [];
+    for (var i = 0; i < command.length; i++) {
+        funcs[i] = (function(index) {
+            return (function() {
+                this.displayField.textContent += command[index];
+            }).bind(this);
+        }).call(this, i);
+    }
+    
+    for (var i = 0; i < command.length; i++) {
+        setTimeout(funcs[i], i * 100);
+    }
+    
+    setTimeout((function() {
+        this.locked = false;
+    }).bind(this), i * 100);
 }
 
 Terminator.prototype.prompt = function(prefix) {
