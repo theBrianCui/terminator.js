99b81b4f3f05ca0b871b58d3b90e2dfcb8b56658
JC/BC <analytalica@gmail.com>
2016-08-07 22:00:14 -0500

    Added wrapper to prompt to focus on click

--- src/terminator.js
+++ src/terminator.js
@@ -1,5 +1,14 @@
 /* Preserve global scope by wrapping all code in a immediately-executed function */
 var Terminator = (function() {
+    let DOMHelper = {
+        append: function (parent, nodes) {
+            for (let i = 0; i < nodes.length; i++) {
+                parent.appendChild(nodes[i]);
+            }
+            return parent;
+        }
+    }
+    
     //Create a new Terminator instance targeting a DOM element with a configuration.
     let Terminator = function(element, config) {
         this.config = {
@@ -29,15 +38,15 @@ var Terminator = (function() {
         this.locked = false; //When locked, user input is disabled.
 
         //Fill in the prompt value with the hiddenField value
-        hiddenField.addEventListener('input', (function() {
+        hiddenField.addEventListener('input', () => {
             console.log('Typing!');
             if (this.displayField && !this.locked) {
                 window.scrollTo(0, document.body.scrollHeight);
                 this.displayField.textContent = this.hiddenField.value;
             }
-        }).bind(this));
+        });
 
-        hiddenField.addEventListener('keydown', (function(e) {
+        hiddenField.addEventListener('keydown', (e) => {
             //Prevent scrolling left and right, which breaks the caret
             //TODO implement a dynamic caret that follows scroll
             if (((e.key == 37) || (e.keyCode == 37)) || ((e.key == 39) || (e.keyCode == 39))) {
@@ -50,17 +59,17 @@ var Terminator = (function() {
                 window.scrollTo(0, document.body.scrollHeight);
                 this.run(this.displayField.textContent);
             }
-        }).bind(this));
+        });
 
         if (this.config.alwaysFocus) {
             //Prevent the hidden field from being blurred
-            hiddenField.addEventListener('blur', (function() {
-                setTimeout((function() {
+            hiddenField.addEventListener('blur', () => {
+                setTimeout(() => {
                     let origX = window.scrollX, origY = window.scrollY;
                     this.hiddenField.focus();
                     window.scrollTo(origX, origY);
-                }).bind(this), 0);
-            }).bind(this), true);
+                }, 0);
+            }, true);
         }
 
         let programs = {};
@@ -161,9 +170,9 @@ var Terminator = (function() {
             let funcs = [];
             for (let i = 0; i < command.length; i++) {
                 funcs[i] = (function(index) {
-                    return (function() {
+                    return () => {
                         this.displayField.textContent += command[index];
-                    }).bind(this);
+                    };
                 }).call(this, i);
             }
 
@@ -171,34 +180,43 @@ var Terminator = (function() {
                 setTimeout(funcs[i], i * 50 + delay);
             }
 
-            setTimeout((function() {
+            setTimeout(() => {
                 this.run(this.displayField.textContent);
-            }).bind(this), command.length * 50 + 100 + delay);
+            }, command.length * 50 + 100 + delay);
         } else {
             console.warn("Terminal currently locked, cannot auto-type command: " + command);
         }
     };
+    
+
 
      /* Build and display the prompt element, then accept input.
      * If a callback is provided, save it for the run command. */
     Terminator.prototype.prompt = function(prefix, callback) {
         this.hiddenField.value = '';
 
-        let promptWrapper = document.createElement('span');
-        promptWrapper.innerHTML = prefix || this.config.prefix;
-        let commandWrapper = document.createElement('span');
-        let caretWrapper = document.createElement('span');
-        caretWrapper.textContent = this.config.caret;
-        caretWrapper.classList.add('terminator-blink');
+        let promptWrapper = document.createElement('div');
+        promptWrapper.addEventListener('click', () => {
+            this.hiddenField.focus();
+        });
 
+        
+        let prompt = document.createElement('span');
+        prompt.innerHTML = prefix || this.config.prefix;
+        
+        let command = document.createElement('span');
+        
+        let caret = document.createElement('span');
+        caret.textContent = this.config.caret;
+        caret.classList.add('terminator-blink');
+        
         /* Append elements to the Terminal in the following order:
-            prompt, command, caret */
+        prompt, command, caret */
+        DOMHelper.append(promptWrapper, [prompt, command, caret]);
         this.element.appendChild(promptWrapper);
-        this.element.appendChild(commandWrapper);
-        this.element.appendChild(caretWrapper);
 
-        this.displayField = commandWrapper;
-        this.activeCaret = caretWrapper;
+        this.displayField = command;
+        this.activeCaret = caret;
         this.callback = callback || null;
         this.locked = false;
         this.hiddenField.focus();
