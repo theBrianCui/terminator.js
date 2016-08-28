b6764356130d26f527b13ee6911111f81be8e4d0
Brian Cui <brianzcui@gmail.com>
2016-08-28 12:52:30 -0500

    Added Constructor tests

--- test/tests.js
+++ test/tests.js
@@ -1,9 +1,14 @@
-QUnit.test("Constructor Tests", function(assert) {
+
+QUnit.module('Constructor Tests');
+QUnit.test('Constructor is a function', function(assert) {
 	assert.ok(typeof Terminator === 'function');
-	
+});
+
+QUnit.test('Constructor consumes target element', function(assert) {
 	var target = document.createElement('div');
 	var main = new Terminator(target);
-	assert.ok(main instanceof Terminator);
-	assert.strictEqual(main.element, target);
-	assert.strictEqual(main.element.innerHTML, '');
+	
+	assert.ok(main instanceof Terminator, 'Constructor produces Terminator Object');
+	assert.strictEqual(main.element, target, 'Constructor consumes target element');
+	assert.strictEqual(main.element.innerHTML, '', 'Target element default state contains nothing');
 });
